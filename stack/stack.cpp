e6329ba619e4c237c389211ab4681c1f4e1528d3
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-05 18:02:56 +0700

    membuat case 4

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -104,5 +104,16 @@ int main()
     case 3:
         stack.peek();
         break;
-    
+    case 4:
+    cout << "Exiting program. " << endl;
+    break;
+default:
+        cout << "Invalid choice. Try again." << endl;
+        break;
+    }
+
+    cout << endl;
+    }
+
+    return 0;
 }
